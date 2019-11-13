b86cdfda9f94392e6aac2922a1c687410423452d
Esten Patrick <estenpatrick@gmail.com>
2019-11-13 16:27:53 -0600

    making categories required on posts

--- studio/schemas/documents/post.js
+++ studio/schemas/documents/post.js
@@ -1,4 +1,4 @@
-import {format} from 'date-fns'
+import { format } from 'date-fns'
 
 export default {
   name: 'post',
@@ -60,7 +60,8 @@ export default {
             type: 'category'
           }
         }
-      ]
+      ],
+      validation: Rule => Rule.required()
     },
     {
       name: 'body',
@@ -105,7 +106,7 @@ export default {
       slug: 'slug',
       media: 'mainImage'
     },
-    prepare ({title = 'No title', publishedAt, slug, media}) {
+    prepare({ title = 'No title', publishedAt, slug, media }) {
       const dateSegment = format(publishedAt, 'YYYY/MM')
       const path = `/${dateSegment}/${slug.current}/`
       return {
